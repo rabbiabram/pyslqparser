e7810c5e59173de14885ae5cf189d75cda94e378
Vladislav Bezverkhiy <rabbiabram@gmail.com>
2013-12-02 20:54:50 +0600

    Did some small refactoring

--- tools/sqloperation.py
+++ tools/sqloperation.py
@@ -1,37 +1,45 @@
 __author__ = 'rabbiabram@gmail.com'
 
 
-class sqloperation:
+class SQLOperation:
     def __init__(self,operation = None):
         self.name = operation
         self.expressions = []
         self.options = []
-    def getOperationName(self):
+
+    def get_operation_name(self):
         return self.name
-    def addExpression(self,expression):
+
+    def add_expression(self,expression):
         self.expressions.append(expression)
-    def getExpressions(self):
+
+    def get_expressions(self):
         return self.expressions
-    def expressionsToArray(self):
+
+    def expressions_to_array(self):
         expr_array = []
         i = 0
         while i < len(self.expressions):
             expr_array.append([ self.expressions[i].getName(), self.expressions[i].getOptions()])
             i = i + 1
         return expr_array
-    def addOption(self,option,prevent_expr = None):
+
+    def add_option(self,option,prevent_expr = None):
         self.options.append([option, prevent_expr])
-    def getPreExpressionOption(self):
+
+    def get_pre_expression_option(self):
         k = -1
         if self.options[0][1] == k:
             return self.options[0][0]
         else:
             return ""
-    def preExpressionOptionExists(self):
+
+    def pre_expression_option_exists(self):
         k = -1
         if len(self.options) > 0:
             return (self.options[0] != None) and (self.options[0][1] == k)
         else:
             return False
-    def expressionExists(self,i):
+
+    def expression_exists(self,i):
         return (i < len(self.expressions)) and (i >= 0)
